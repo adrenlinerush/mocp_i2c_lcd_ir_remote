c5f635a7bbd734ab4dae367945001fe3260a91d1
Austin Mount <austin@adrenlinerush.net>
2023-05-07 20:35:48 -0400

    add support for stop play and play from stopped state

--- mocp_ir_daemon.py
+++ mocp_ir_daemon.py
@@ -17,9 +17,14 @@ def readInputEvent(device):
             data = categorize(event)
             if data.keystate==data.key_down:
               reponse = data.keycode
+              mocp_info = subprocess.check_output(['mocp', '-i']).decode('ascii').split('\n')
+              mocp_status = mocp_info[0].split(':')[1].strip()
               match (data.keycode):
                 case "KEY_PLAYPAUSE":
-                  response = subprocess.check_output(['mocp', '-G']) 
+                  if mocp_status == 'STOP':
+                    response = subprocess.check_output(['mocp', '-p']) 
+                  else:
+                    response = subprocess.check_output(['mocp', '-G']) 
                 case "KEY_BACK":
                   response = subprocess.check_output(['mocp', '-r']) 
                 case "KEY_FORWARD":
@@ -30,6 +35,9 @@ def readInputEvent(device):
                   response = subprocess.check_output(['mocp', '--volume=-5']) 
                 case "KEY_SHUFFLE":
                   response = subprocess.check_output(['mocp', '--toggle=shuffle']) 
+                case "KEY_POWER":
+                  response = subprocess.check_output(['mocp', '-s']) 
+
 
 while True:
     readInputEvent(irin)
